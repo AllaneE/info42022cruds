2b4f40f35026a7dd5edecf1c6337e941134c8048
matheus <abqmatheus7@gmail.com>
2022-08-31 11:45:13 -0300

    criando banco de dados

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
+from mailbox import NotEmptyError
 from django.db import models
 
 # Create your models here.
+class Camisas(models.Model):
+    tamanho = models.CharField()
+    marca = models.CharField()
+    tipo = models.CharField()
+    nome = models.CharField()
+    ano = models.DateField()
+    pernosalizacao = models.IntegerField("numero")
